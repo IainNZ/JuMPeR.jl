05a398b678b07245945356d92e63886feefaf8ed
Iain Dunning <iaindunning@gmail.com>
2014-09-19 16:20:15 -0400

    Fix for JuMP 0.6.1

--- src/robustmacro.jl
+++ src/robustmacro.jl
@@ -1,101 +1,104 @@
 macro defUnc(m, x, extra...)
+    #-------- DIRECT COPY BEGIN ------- (except 'variable' -> 'uncertain')
     m = esc(m)
+    # Identify the variable bounds. Four (legal) possibilities are "x >= lb",
+    # "x <= ub", "lb <= x <= ub", or just plain "x"
     if isexpr(x,:comparison)
-        # we have some bounds
-        if x.args[2] == :>=
+        # We have some bounds
+        if x.args[2] == :>= || x.args[2] == :≥
             if length(x.args) == 5
-                error("Use the form lb <= unc <= ub instead of ub >= unc >= lb")
+                # ub >= x >= lb
+                x.args[4] == :>= || x.args[4] == :≥ || error("Invalid uncertain bounds")
+                var = x.args[3]
+                lb = esc(x.args[5])
+                ub = esc(x.args[1])
+            else
+                # x >= lb
+                var = x.args[1]
+                @assert length(x.args) == 3
+                lb = esc(x.args[3])
+                ub = Inf
             end
-            @assert length(x.args) == 3
-            # lower bounds, no upper
-            lb = esc(x.args[3])
-            ub = Inf
-            var = x.args[1]
-        elseif x.args[2] == :<=
+        elseif x.args[2] == :<= || x.args[2] == :≤
             if length(x.args) == 5
                 # lb <= x <= u
-                lb = esc(x.args[1])
-                if (x.args[4] != :<=)
-                    error("Expected <= operator")
-                end
-                ub = esc(x.args[5])
                 var = x.args[3]
+                (x.args[4] != :<= && x.args[4] != :≤) &&
+                    error("in @defUnc ($var): expected <= operator after uncertain name.")
+                lb = esc(x.args[1])
+                ub = esc(x.args[5])
             else
-                # x <= u
+                # x <= ub
+                var = x.args[1]
+                # NB: May also be lb <= x, which we do not support
+                #     We handle this later in the macro
+                @assert length(x.args) == 3
                 ub = esc(x.args[3])
                 lb = -Inf
-                var = x.args[1]
             end
+        else
+            # Its a comparsion, but not using <= ... <=
+            error("in @defUnc ($(string(x))): use the form lb <= ... <= ub.")
         end
     else
+        # No bounds provided - free variable
+        # If it isn't, e.g. something odd like f(x), we'll handle later
         var = x
         lb = -Inf
         ub = Inf
     end
+    #-------- DIRECT COPY END ------- 
     
+    #-------- DIRECT COPY BEGIN ------- 
+    # - except 'variable' -> 'uncertain'
+    # - except no semicont, semiint
+    # - except column format
     t = :Cont
+    gottype = 0
     if length(extra) > 0
-        gottype = 0
-        if extra[1] == :Int || extra[1] == :Bin
+        if extra[1] in [:Bin, :Int]
             gottype = 1
-            if extra[1] == :Int
-                t = :Int
+            t = extra[1]
+        end
+
+        if t == :Bin 
+            if (lb != -Inf || ub != Inf) && !(lb == 0.0 && ub == 1.0)
+            error("in @defUnc ($var): bounds other than [0, 1] may not be specified for binary uncertains.\nThese are always taken to have a lower bound of 0 and upper bound of 1.")
             else
-                if lb != -Inf || ub != Inf
-                    error("Bounds may not be specified for binary variables. These are always taken to have a lower bound of 0 and upper bound of 1.")
-                end
-                t = :Int
                 lb = 0.0
                 ub = 1.0
             end
         end
+
+        gottype == 0 &&
+            error("in @defUnc ($var): syntax error")
     end
+    #-------- DIRECT COPY END ------- 
 
-
+    #-------- DIRECT COPY BEGIN ------- 
+    # - except Variable -> Uncertain
     if isa(var,Symbol)
-        # easy case
+        # Easy case - a single uncertain
         return quote
             $(esc(var)) = Uncertain($m,$lb,$ub,$(Meta.quot(t)),$(string(var)))
             nothing
         end
-    else
-        if !isexpr(var,:ref)
-            error("Syntax error: Expected $var to be of form var[...]")
-        end
-        varname = esc(var.args[1])
-        idxvars = {}
-        idxsets = {}
-        refcall = Expr(:ref,varname)
-        for s in var.args[2:end]
-            if isa(s,Expr) && s.head == :(=)
-                idxvar = esc(s.args[1])
-                idxset = esc(s.args[2])
-            else
-                idxvar = gensym()
-                idxset = esc(s)
-            end
-            push!(idxvars, idxvar)
-            push!(idxsets, idxset)
-            push!(refcall.args, idxvar)
-        end
-        code = :( $(refcall) = Uncertain($m, $lb, $ub, $(Meta.quot(t))) )
-        for (idxvar, idxset) in zip(reverse(idxvars),reverse(idxsets))
-            code = quote
-                for $idxvar in $idxset
-                    $code
-                end
-            end
-        end
-        
-        mac = Expr(:macrocall,symbol("@gendict"),varname,:Uncertain,idxsets...)
-        addDict = :( push!($(m).ext[:Robust].dictList, $varname) )
-        code = quote 
-            $mac
-            $code
-            $addDict
-            nothing
-        end
-        return code
+    end
+    @assert isa(var,Expr)
+    #-------- DIRECT COPY END ------- 
+
+
+    #-------- DIRECT COPY BEGIN ------- 
+    # - Variable -> Uncertain
+    # - m.dictlist -> m.ext[:Robust].dictList
+    refcall, idxvars, idxsets, idxpairs = JuMP.buildrefsets(var)
+    code = :( $(refcall) = Uncertain($m, $lb, $ub, $(Meta.quot(t))) )
+    looped = JuMP.getloopedcode(var, code, :(), idxvars, idxsets, idxpairs, :Uncertain)
+    varname = esc(JuMP.getname(var))
+    return quote 
+        $looped
+        push!($(m).ext[:Robust].dictList, $varname)
+        $varname
     end
 end
 
